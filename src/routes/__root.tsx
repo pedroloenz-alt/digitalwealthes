61ea2a4940725d8b517fca11f934bf960c8c793b
Loenz <pedrozueiro8@gmail.com>
2026-09-02 18:51:19 -0300

    Add TikTok analytics script to __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -105,6 +105,7 @@ function RootShell({ children }: { children: ReactNode }) {
     <html lang="en">
       <head>
         <HeadContent />
+        <script dangerouslySetInnerHTML={{ __html: "!function (w, d, t) {\n  w.TiktokAnalyticsObject = t;\n  var ttq = w[t] = w[t] || [];\n  ttq.methods = [\"page\", \"track\", \"identify\", \"instances\", \"debug\", \"on\", \"off\", \"once\", \"ready\", \"alias\", \"group\", \"enableCookie\", \"disableCookie\", \"holdConsent\", \"revokeConsent\", \"grantConsent\"];\n  ttq.setAndDefer = function (t, e) { t[e] = function () { t.push([e].concat(Array.prototype.slice.call(arguments, 0))) }; };\n  for (var i = 0; i < ttq.methods.length; i++) ttq.setAndDefer(ttq, ttq.methods[i]);\n  ttq.instance = function (t) { var e = ttq._i[t] || []; for (var n = 0; n < ttq.methods.length; n++) ttq.setAndDefer(e, ttq.methods[n]); return e; };\n  ttq.load = function (e, n) { var r = \"https://analytics.tiktok.com/i18n/pixel/events.js\", o = n && n.partner; ttq._i = ttq._i || {}; ttq._i[e] = []; ttq._i[e]._u = r; ttq._t = ttq._t || {}; ttq._t[e] = +new Date; ttq._o = ttq._o || {}; ttq._o[e] = n || {}; n = document.createElement(\"script\"); n.type = \"text/javascript\"; n.async = !0; n.src = r + \"?sdkid=\" + e + \"&lib=\" + t; e = document.getElementsByTagName(\"script\")[0]; e.parentNode.insertBefore(n, e); };\n  ttq.load(\"D97HJD3C77U133LML5O0\");\n  ttq.page();\n}(window, document, \"ttq\");" }} />
       </head>
       <body>
         {children}
